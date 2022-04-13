b5e027ca2cd90bacf8e3acb11bd10bed2e151213
Fernanda Gastal <54810545+fegastal@users.noreply.github.com>
2022-04-13 17:04:23 -0300

    Update ex04_MAC.sh

--- ex04_MAC.sh
+++ ex04_MAC.sh
@@ -1,1 +1,5 @@
+Escreva uma linha de comando que mostre os endereços MAC da sua máquina. Cada endereço deve ser seguido por uma quebra de linha.
+
+Um comando que pode ser utilizado é:
+
 ifconfig | grep "ether" | awk '{print $2}'
